e939d75a76d5b454d8445ad631a8df1f6716c88a
IloveGreeny <greenxd23@gmail.com>
2022-04-23 00:27:46 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import Header from "./componets/Header";
+import Footer from "./componets/Footer";
+
 
 export default function App() {
   return (
     <div className="App">
+      <Header/>
+      <img src="http://webmadewell.com/wp-content/uploads/2018/01/img-bg-sample-parallax-header.jpg" alt="ssky" width="100%" height="500px"/>
 
     </div>
   );
